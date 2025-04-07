221ea0893321ee068c62ad3eddac4e653d02936b
Evgenii Novikov <disregard.the.rest@gmail.com>
2025-04-07 21:42:47 +0300

    export update

--- lib/nit_riverpod_notifications.dart
+++ lib/nit_riverpod_notifications.dart
@@ -1,7 +1,7 @@
 library nit_riverpod_notifications;
 
+export 'src/nit_notification_listener_widget.dart';
 export 'src/nit_notifications/nit_notification.dart';
 export 'src/nit_notifications/nit_notification_types_enum.dart';
-
 export 'src/ref_extension.dart';
-export 'src/nit_notification_listener_widget.dart';
+export 'src/state/nit_notifications_state.dart';
