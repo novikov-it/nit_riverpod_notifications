2de7e2facc53a678a78878c7e5b587eba47a22b5
Strelkovik <135411036+Strelkovik@users.noreply.github.com>
2025-07-18 04:14:54 +0200

    Fix

--- lib/nit_riverpod_notifications.dart
+++ lib/nit_riverpod_notifications.dart
@@ -5,8 +5,5 @@ export 'src/nit_notifications/nit_notification.dart';
 export 'src/nit_notifications/nit_notification_types_enum.dart';
 export 'src/ref_extension.dart';
 export 'src/state/nit_notifications_state.dart';
-export 'src/nit_notification_listener_widget.dart';
-export 'src/nit_notifications/nit_notifications_default_flash_widget.dart';
 
-export 'src/state/nit_notifications_state.dart';
 export 'package:flash/flash.dart' show FlashPosition, FlashBehavior;
